a7426df96f8100d38addf2d806d7237400095674
wesam <108880473+wesamdev@users.noreply.github.com>
2023-10-29 22:45:36 +0400

    commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import json
 
 app = Flask(__name__)
@@ -17,8 +17,13 @@ def search():
 
     # Perform a simple search
     results = [camp for camp in data['companies'] if query.lower() in camp['name'].lower()]
+    print(results)
 
     return render_template('search_results.html', results=results)
+
+@app.route('/search', methods=['GET'])
+def search_redirect():
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True)
